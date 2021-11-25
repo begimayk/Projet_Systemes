51bed8486254749c23d139ede0db011bcd67fb46
begimayk <78308626+begimayk@users.noreply.github.com>
2021-11-25 20:06:32 +0100

    Create table.h

---
+++ src/table.h
@@ -0,0 +1,19 @@
+//Begimay KONUSHBAEVA
+//Joel KONGOLO
+#ifndef TABLE_H 
+#define TABLE_H
+typedef struct _Table_entry { 
+    int key;
+    char* val;
+    struct _Table_entry *next; 
+    } Table_entry;
+// cle
+// valeur
+// suivant
+typedef Table_entry *PTable_entry;
+
+char* lookup(PTable_entry table, int k);
+
+void store(PTable_entry *table, int k, char v[]); 
+void display(PTable_entry table);
+#endif
